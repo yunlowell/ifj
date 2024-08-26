0b52fb5229807fec52b9c1c78e7904a7471c577c
SH_kim <dal2018@naver.com>
2024-08-26 14:36:27 +0900

    feat: url편의성, 프로필오류 수정
    - http://127.0.0.1:8000/ 주소를 사용하면 /products (게시판) 으로 이동
    - accounts/views.py redirect 수정

--- spartamarket/urls.py
+++ spartamarket/urls.py
@@ -18,13 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include("users.urls")),
     path('accounts/', include("accounts.urls")),
     path('products/', include("products.urls")),
+    path('', lambda request: redirect('products/'))
 ]
 
 if settings.DEBUG:

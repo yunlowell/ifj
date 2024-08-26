a4f563ee15e91123304af023f1dc445e65bd3f57
yun <dbsdbf95@gmail.com>
2024-08-27 00:28:59 +0900

    feat: create css 적용, 취소버튼 생성, 제목 내용 빈칸일 때 안내문구 생성

--- products/forms.py
+++ products/forms.py
@@ -6,3 +6,14 @@ class ArticleForm(forms.ModelForm):
         model = Article
         fields = "__all__"
         exclude = ("author", "like_users",)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # 제목 필드에 placeholder 추가
+        self.fields['title'].widget.attrs.update({
+            'placeholder': '제목을 입력하세요'
+        })
+        # 내용 필드에 placeholder 추가
+        self.fields['content'].widget.attrs.update({
+            'placeholder': '내용을 입력하세요'
+        })

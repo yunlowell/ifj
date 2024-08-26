48891d133474b2da23c6447b0b36cec62abbfb82
lguewk06 <lguewk06@gmail.com>
2024-08-26 11:02:28 +0900

    유저기능 merge

--- accounts/models.py
+++ accounts/models.py
@@ -2,5 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
+
 class User(AbstractUser):
-    profile_image = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    profile_image = models.ImageField(
+        upload_to='profile_pics/', blank=True, null=True)
+    followers = models.ManyToManyField(
+        'self', symmetrical=False, related_name='following')

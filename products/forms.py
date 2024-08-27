54a0c6713161db048e32f38f217651ce4c5c84e0
SH_kim <dal2018@naver.com>
2024-08-27 11:15:10 +0900

    feat: 글상세페이지 css
    -create.html 수정
    -forms.py 위젯추가

--- products/forms.py
+++ products/forms.py
@@ -6,3 +6,10 @@ class ArticleForm(forms.ModelForm):
         model = Article
         fields = "__all__"
         exclude = ("author", "like_users",)
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '제목을 입력하세요'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 6, 'placeholder': '내용을 입력하세요'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': '가격을 입력하세요'}),
+            'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+        }
